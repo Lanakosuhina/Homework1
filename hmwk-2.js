7121659655826d5e7680e7cbcbbabe1eabb451b2
Lanakosuhina <lana.kosuhina@gmail.com>
2023-08-30 18:06:47 +0300

    Edited homework

--- hmwk-2.js
+++ hmwk-2.js
@@ -1,21 +1,19 @@
 // задание 1
-let a1 = 10;
-alert(a1);
-
-let a2 = 20; 
-alert(a2);
+let a = 10;
+a = 20
+alert(a);
 
 // задание 2
 let firstIPhone = 2007;
 alert(firstIPhone);
 
 // задание 3
-let creatorJs = (`Брендан Эйх`);
+let creatorJs = `Брендан Эйх`;
 alert(creatorJs);
 
 // задание 4
-let a = 10;
-let b = 2;
+let b = 10;
+let c = 2;
 alert(`
 Сумма: ${a + b}
 Разность: ${a - b}
@@ -30,8 +28,8 @@ alert (`
 `)
 
 // задание 6
-let c = 9;
-let d = 2;
+let d = 9;
+let e = 2;
 alert (`
 Остаток от деления: ${9 % 2}
 `)
@@ -55,15 +53,16 @@ let user = {
     name: "Sveta",
     age: 21,
     isAdmin: "Хотите продолжить?",
-    cityOfResidence: "Saint Petersburg",
 };
+user["city of residence"] = true;
+alert (user["city of residence"]);
+
 user.age = 22;
 console.log(user);
 
 delete user.cityOfResidence; 
-   
-user.info = true;
-let info = prompt("Какую информацию хотите узнать о пользователе?");
+
+let info = prompt("Какую информацию хотите узнать о пользователе?", "name","age", "isAdmin");
 alert(user[info]);
 
 // задание 10
