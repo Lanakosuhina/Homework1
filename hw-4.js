f6adf36e9252712506c6fb03514e59984c5c7a06
Lanakosuhina <lana.kosuhina@gmail.com>
2023-09-03 20:46:26 +0300

    homework four is done

--- hw-4.js
+++ hw-4.js
@@ -1,5 +1,45 @@
 // task 1
-// let i = 2;
-// while (i == 2) {
-    // console.log('Привет!');
-// }
+for (let i = 0; i < 2; i++) {
+    console.log('Привет!');
+}
+
+// task 2
+let code = 1;
+while (code < 6) {
+    console.log(code);
+    code++;
+}
+
+// task 3 
+for (let fig = 7; fig <= 22; fig++) {
+    console.log(fig);
+}
+
+// task 4 
+let obj = {
+    Коля: 200,
+    Вася: 300,
+    Петя: 400,
+}
+for (const key in obj) {
+    const cash = obj[key];
+    console.log(`${key} — зарплата ${cash} долларов`);
+}
+
+// task 5
+let n = 1000;
+let num = 0;
+
+while (n >= 50) {
+    n /= 2;
+    num++;
+}
+console.log(n);
+console.log(num)
+
+// task 6
+
+
+for (let friday = 4; friday <= 31 ; friday += 7) {
+    console.log(`Сегодня пятница,${friday}-е число. Необходимо подготовить отчет.`);
+}
